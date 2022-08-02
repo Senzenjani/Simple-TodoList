7d93b25f7c56d626bc9e1d259748f8b954f379f7
Senzenjani <smanjawira@gmail.com>
2022-08-02 15:24:59 +0200

    Added task  todolist

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id='app'>
+
+  <div class='col-12'>
+    <div class='cardBox'>
+      <div class ='container'>
+        <h2>My Tasks</h2>
+        <ul class='taskList'>
+          <li v-for='(taskItem, index) in taskList' 
+           :key='`${index}_${Math.random()}`'>
+            
+            <input type='checkbox' :checked='!!taskItem.finishedAt'
+              @input='changeStatus(index)'/>
+             {{ taskItem.task }}
+            
+            <span v-if='taskItem.finishedAt'>
+             {{ taskItem.finishedAt }}
+          </span>
+          </li>
+       
+        </ul>
+      </div>
+    </div>
+  </div>
+
+    <current-time class='col-4'/>
+  <task-input class='col-6'
+  @add-task="addNewTask" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrentTime from './components/CurrentTime.vue'
+import TaskInput from './components/TaskInput.vue'
 
 export default {
-  name: 'App',
+  name: 'TodoApp',
   components: {
-    HelloWorld
+    CurrentTime,
+    TaskInput
+  },
+  data:() => ({
+    taskList: [],
+  }), 
+  methods:{
+    addNewTask(task){
+      this.taskList.push({
+        task, 
+        createdAt: Date.now(),
+        finishedAt: undefined
+      })
+    },
+    changeStatus(taskIndex){
+      const task  = this.taskList[taskIndex];
+      if(task.finishedAt){
+        task.finishedAt = undefined;
+      }else{
+        task.finishedAt = Date.now();
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.taskList li{
+text-align: left;
 }
 </style>
